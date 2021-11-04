97ef7c281583d4c9e6778cf661ea2332d2eac107
d-rov <duncan.rov@gmail.com>
2021-11-03 23:35:40 -0600

    project setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 
 function App() {
+  // GET one plant call
+  // GET all plants call
+  // POST plant call
+  
   return (
     <div className="App">
     </div>
--- server.ts
+++ server.ts
@@ -1,6 +1,17 @@
-import express from 'express'
-
+const express = require('express')
 const app = express()
+
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// database connection
+// mongodb
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
 
 // app GET one plant
 // app GET all plants
